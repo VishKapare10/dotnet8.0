f7feaa81528bba559a3d6fa37d92de1fb8c934f2
VishKapare10 <vish10kapare@gmail.com>
2024-10-02 22:07:19 +0530

    CustomMiddlewareDemo changes.

--- CustomMiddlewareDemo/Program.cs
+++ CustomMiddlewareDemo/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddLogging();
+
 var app = builder.Build();
 
 // Use the custom middleware
 app.UseMiddleware<RequestTimingMiddleware>();
 
+// Define a simple endpoint
 app.MapGet("/", () => "Hello, World!");
 
 app.Run();
